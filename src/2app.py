ac079b3d1707395dac13192395621cbf4ff692d0
aumthakkar <aumthakkar@yahoo.co.uk>
2025-11-06 09:04:39 +0000

    corrected syntax error from 2app.py

--- src/2app.py
+++ src/2app.py
@@ -23,7 +23,7 @@ def healthz():
     })
 
 # main driver function
-if __name__ == '__main__':👍🏽
+if __name__ == '__main__':
 
     # run() method of Flask class runs the application 
     # on the local development server.
